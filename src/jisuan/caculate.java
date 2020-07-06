0e83acdf587a911a2ab2f4cdd9821b340421e021
WXY <2423144591@qq.com>
2020-07-06 13:36:07 +0800

    取出操作数完成

--- src/jisuan/caculate.java
+++ src/jisuan/caculate.java
@@ -1,4 +1,21 @@
 package jisuan;
-
+import java.math.BigDecimal;
 public class caculate {
+    private String firstNum = "0";
+    private  String seconeNum ="0";
+    private  char opertor ='+';
+    private  String result ="0";
+    public String getFirstNum(){
+        return firstNum;
+    }
+    private String getSeconeNum(){
+        return seconeNum;
+    }
+    public void setFirstNum(String firstNum){
+        this.firstNum=firstNum;
+    }
+    public void setSeconeNum(String seconeNum){
+        this.seconeNum=seconeNum;
+    }
+
 }
