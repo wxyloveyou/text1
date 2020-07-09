c16f96a77d03d2be8ee7f280e572587dc7d4c5e1
WXY <2423144591@qq.com>
2020-07-09 21:26:17 +0800

    计算功能实现，

--- src/jisuan/caculate.java
+++ src/jisuan/caculate.java
@@ -1,57 +1,61 @@
 package jisuan;
+
 import java.math.BigDecimal;
 public class caculate {
     private String firstNum = "0";
-    private  String seconeNum ="0";
-    private  char opertor ='+';
-    private  String result ="0";
-    public String getFirstNum(){
+    private char operator = '+';
+    private String secondNum = "0";
+    private String result="0";
+    public String getFirstNum() {
         return firstNum;
     }
-    private String getSeconeNum(){
-        return seconeNum;
+    public void setFirstNum(String firstNum) {
+        this.firstNum = firstNum;
     }
-    public void setFirstNum(String firstNum){
-        this.firstNum=firstNum;
+    public char getOperator() {
+        return operator;
     }
-    public void setSeconeNum(String seconeNum){
-        this.seconeNum=seconeNum;
+    public void setOperator(char operator) {
+        this.operator = operator;
     }
-    public char getOpertor(){
-        return opertor;
+    public String getSecondNum() {
+        return secondNum;
     }
-    public void setOpertor(char operator){
-        this.opertor = operator;
+    public void setSecondNum(String secondNum) {
+        this.secondNum = secondNum;
     }
-    public String getResult(){
+    public String getResult() {
         return result;
     }
-    public void setResult(){
-        this.result=result;
+    public void setResult(String result) {
+        this.result = result;
     }
-    public void calculate(){
-        BigDecimal first = new BigDecimal(this.firstNum);
-        BigDecimal second = new BigDecimal(this.seconeNum);
-        switch (this.opertor){
-            case '+':{
-                this.result=first.add(second).toString();
-                break;
-            }
-            case :'-'{
-                this.result=first.subtract(second).toString();
-                break;
-            }
-            case :'*'{
-                this.result=first.multiply(second).toString();
-                break;
-            }
-            case :'/'{
-                if (second.doubleValue()==0){
-                    throw new RuntimeException("被除数为了0，NULL")；
-                }
-                this.result=first.divide(second).toString();
-            }
 
+    public void calculate() {
+        BigDecimal first = new BigDecimal(this.firstNum);
+        BigDecimal second = new BigDecimal(this.secondNum);
+        switch(this.operator) {
+            case '+':{
+                this.result = first.add(second).toString();
+                break;
+            }
+            case '-':{
+                this.result = first.subtract(second).toString();
+                break;
+            }
+            case '*':{
+                this.result = first.multiply(second).toString();
+                break;
+            }
+            case '/':{
+                if(second.doubleValue() == 0) {
+                    throw new RuntimeException("被除数为0,null");
+                }
+                this.result = first.divide(second).toString();
+                break;
+            }
+            default:
+                throw new RuntimeException("运算符只能是+-*/");
         }
     }
 }
